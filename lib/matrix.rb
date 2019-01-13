37f291c5b84a913308e8404281890eb1d18fd7ba
Sweta Tiwari <swetavtiwari62189@gmail.com>
2019-01-13 22:18:00 +0530

    modify the function 'rows' to return the first row a matrix

--- lib/matrix.rb
+++ lib/matrix.rb
@@ -1,9 +1,19 @@
 class Matrix
   def initialize(matrix_elements)
+    row_number = 0
+    @mat = Array.new
+    (matrix_elements.split "\n").each do |row|
+      @mat[row_number] = row
+      row_number += 1
+    end
   end
 
   def rows(row_number)
-    [1, 2]
+    row = Array.new
+    (@mat[0].split " ").each do |element|
+      row << element.to_i
+    end
+    row
   end
 end
 
