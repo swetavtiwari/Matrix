a82d4fd37b487010eb3b802fcef7ab937bd73dbb
Sweta Tiwari <swetavtiwari62189@gmail.com>
2019-01-13 22:16:43 +0530

    add another test for returning the first row of a matrix

--- spec/matrix_spec.rb
+++ spec/matrix_spec.rb
@@ -5,5 +5,9 @@ describe 'matrix.rb' do
       matrix = Matrix.new("1 2\n 10 20")
       expect(matrix.rows(0)).to eq [1, 2]
     end
+    it 'return [9,7]  for the input "9 7\n8 6"' do
+      matrix = Matrix.new("9 7\n8 6")
+      expect(matrix.rows(0)).to eq [9, 7]
+    end
   end
 end
